63d77c1481b14c20f82bd9d828404554e2dffc36
yaramdotnet <yaramdotnet@outlook.com>
2016-08-18 15:39:12 +0530

    After props lesson

--- app/index.js
+++ app/index.js
@@ -5,13 +5,44 @@ var HelloWorld = React.createClass({
 	render : function(){
 
 		return (
-			<div> Hello World React JS Program </div>
+			<div> Hello World React JS Program {this.props.name} </div>
 		)
 
 	}
 });
 
+var FriendsContainer = React.createClass({
+   render: function(){
+   	var name = 'Sam';
+   	var friends_names = ["Ram","Lakshman","Bharath"];
+   	return (
+   		<div>
+   		<HelloWorld name="Jags" />
+   		<h3>Name: {name} </h3>
+   		<ShowList friends={friends_names}/>   		
+   		</div>
+   	)
+   }
+});
+
+var ShowList   = React.createClass({
+	
+
+	render: function(){
+		var friend_list = this.props.friends.map(function(friend){
+		return <li>{friend}</li>
+		});
+		return(
+			<ul>
+				{friend_list}
+			</ul>
+		)
+
+	}
+
+});
+
 ReactDOM.render(
-	<HelloWorld />,
+	<FriendsContainer />,	
 	document.getElementById('app')
 );
